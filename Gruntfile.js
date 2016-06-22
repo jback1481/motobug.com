18f42a0119a59ac99e4181ab437249f93b1f1d3e
Joel Back <jback@tpt.org>
2016-06-22 13:31:49 -0500

    Moved foundation vendor directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,78 +1,86 @@
+(function () { 'use strict'; }());
+
 module.exports = function(grunt) {
-  // -----------------------------------------
-  // Start Grunt configuration
-  // -----------------------------------------
+  // Read in the config file
+  var globalConfig = grunt.file.readJSON('config.json');
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
-
-    // Load package.json file
     pkg: grunt.file.readJSON('package.json'),
 
-    // --------------------------------------
-    // watch Configuration
-    // --------------------------------------
-
-    watch: {
-      grunt: {
-        files: ['Gruntfile.js'],
-        tasks: ['default']
-      },
-
-      sass: {
-        files: 'develop/scss/**/*.scss',
-        tasks: ['buildCss']
-      }
-    },
-
-    // --------------------------------------
-    // sass Configuration
-    // --------------------------------------
-
-    sass: {
+    jshint: {
       options: {
-        loadPath: ['develop/scss/vendor']
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true
+        }
       },
-      dist: {
+      files: [
+        'gruntfile.js',
+        'js/src/*.js',
+        'js/src/vendor/*.js'
+      ]
+    },
+
+    concat: {
+      options: {
+
+      },
+      concatJS: {
         options: {
-          sourcemap: 'none',
-          style:     'nested'
+          // define a string to put between each file in the concatenated output
+          separator: ';'
         },
-        files: [{
-          expand: true,
-          cwd:    'develop/scss',
-          src:    ['*.scss'],
-          dest:   'dist/assets/css',
-          ext:    '.css'
-        }]
+        dist: {
+          // the files to concatenate
+          src: ['src/**/*.js'],
+          // the location of the resulting JS file
+          dest: 'dist/<%= pkg.name %>.js'
+        }
+      },
+      concatCSS: {
+        options: {
+          // define a string to put between each file in the concatenated output
+          separator: ';'
+        },
+        dist: {
+          // the files to concatenate
+          src: ['src/**/*.css'],
+          // the location of the resulting JS file
+          dest: 'dist/<%= pkg.name %>.css'
+        }
       }
     },
 
-    // --------------------------------------
-    // cssmin Configuration
-    // --------------------------------------
+    uglify: {
+      options: {
 
-    cssmin: {
-      minify: {
-        src: 'dist/assets/css/style.css',
-        dest: 'css/style.min.css'
+      },
+      uglifyJS: {
+        options: {
+
+        }
+      },
+      uglifyCSS: {
+        options: {
+
+        }
       }
+    },
+    
+    compass: {
+    },
+    
+    watch: {
     }
+
   });
 
-  // -----------------------------------------
-  // Load Grunt tasks
-  // -----------------------------------------
-
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-  // -----------------------------------------
-  // Register Grunt tasks
-  // -----------------------------------------
-
-  grunt.registerTask('buildCSS', ['sass', 'cssmin']);
-  grunt.registerTask('buildJS',  ['concat', 'uglify']);
-  grunt.registerTask('default', ['buildCss']);
+  grunt.registerTask('default', ['jshint', 'concatJS', 'uglify', 'concatCSS', 'compass']);
 };
